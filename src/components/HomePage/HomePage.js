b35201c9e73e1e6d08b979787148501a1802d46a
tonplamm <tonplam_kanokpol@hotmail.com>
2021-02-20 11:11:59 +0700

    feat: user interface

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -5,21 +5,53 @@ class HomePage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      appIconText: "loading icon",
+      appIconText: 'loading icon',
       data: null,
+      name: "",
+      server: "",
+      sumbit: false,
+      message: null,
     };
+  }
+
+  updateName = (e) => {
+    this.setState({name: e.target.value});
+  }
+
+  updateServer = (e) => {
+    this.setState({server: e.target.value});
+  }
+
+  updateMessage = (e) => {
+    this.setState({message: e.target.value});
+  }
+
+  clickConnect = (e) => {
+    this.setState({sumbit: true});
+  }
+
+  clickSend = (e) => {
+    // todo:
   }
 
   render() {
     const source = new EventSource('https://chat-room-be.herokuapp.com/message');
     source.onmessage = function logEvents(event) {
       console.log(JSON.parse(event.data))
-      console.log(event.data);   
+      console.log(event.data);
     }
 
     return (
-      <div className="app">
-        <div>{this.state.data}</div>
+      <div className='app' style={{textAlign: 'left', marginTop: '16px'}}>
+        User: <input type='text' id='name' name='name' onChange={this.updateName} className='userInput'></input>
+        Chat server: <input type='text' id='server' name='server' onChange={this.updateServer} className='serverInput'></input>
+        <button onClick={this.clickConnect}>connect</button>
+        
+        <div className={`plam ${this.state.sumbit ? 'show' : 'hide'}`}>data: {this.state.data}</div>
+        <div className='messageInputContainer'>
+          Message: <input type='text' id='name' name='name' onChange={this.updateMessage} className='messageInput'></input>
+          <button disabled={!this.state.message} onClick={this.clickSend}>send</button>
+        </div>
       </div>
     );
   }
